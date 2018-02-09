222ee3020d8ca538c27597ceb1846839fb4cefc9
Vinothkumar7237 <35688289+Vinothkumar7237@users.noreply.github.com>
2018-02-09 14:28:09 +0530

    intervaleven22vinot09.c

---
+++ intervaleven22vinot09.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2,i,odd;
+printf("enter the interval");
+scanf("%d%d",&num1,&num2);
+for(i=num1;i<num2;i++)
+{
+odd=i%2;
+if(odd==0)
+{
+printf("%d",i);
+}
+}
+return 0;
+}
