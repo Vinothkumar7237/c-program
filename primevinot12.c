9364f2c4016c3b48001070ffef72819672331aca
Vinothkumar7237 <35688289+Vinothkumar7237@users.noreply.github.com>
2018-02-12 16:00:46 +0530

    Update primevinot12.c

--- primevinot12.c
+++ primevinot12.c
@@ -1,6 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-int num1,num2,i;
-printf("enter the intervals");
-scanf("%d%d",&num1,&num2);
+    int num2, num1, i, flag;
+    printf("Enter two numbers(intervals): ");
+    scanf("%d %d", &num1, &num2);
+
+    while (num1 <num2)
+    {
+        flag = 0;
+
+        for(i = 2; i <= num1/2; ++i)
+        {
+            if(num1 % i == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)
+            printf("%d ", num1);
+
+        ++num1;
+    }
+
+    return 0;
+}
