ad942ac102e96ef1955a84f989fc7b1164bcf920
Vinothkumar7237 <35688289+Vinothkumar7237@users.noreply.github.com>
2018-02-28 16:04:52 +0530

    Update viontstring27.c

--- viontstring27.c
+++ viontstring27.c
@@ -3,11 +3,11 @@
 #include<ctype.h>
 int main(void) 
 {
-int len,digit=0;
+int a,digit=0;
 char str[10];
 scanf("%s",str);
-len=strlen(str);
-for(int i=0;i<len;i++)
+a=strlen(str);
+for(int i=0;i<a;i++)
 {
 if(isdigit(str[i]))
 digit=0;
