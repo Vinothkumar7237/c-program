efeb8b70e70cacede0c4e6fa901526e209488545
Vinothkumar7237 <35688289+Vinothkumar7237@users.noreply.github.com>
2018-03-09 13:57:23 +0530

    vinotcompare9.c

---
+++ vinotcompare9.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int a,b;
+    char a[10],y[10];
+    printf("enter the string ");
+    scanf("%s",&a);
+    printf("enter the string ");
+    scanf("%s",&b);
+    x=strlen(a);
+    b=strlen(b);
+    if(x>=y)
+    {
+        printf("%s",a);
+    }
+    else
+    {
+        printf("%s",b);
+    }
+}
